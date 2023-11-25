deecc2026a4b10c73940f4aa3113397f7bbe8627
NishantSingh02 <nishant.singh@taxmanntechnologies.com>
2023-11-25 16:35:33 +0530

    cors origins added

--- src/index.js
+++ src/index.js
@@ -9,7 +9,10 @@ const port = process.env.PORT || 3000;
 
 const corsOptions = {
   origin: [
-    "http://localhost", // add more origins
+    "http://localhost:5173",
+    "http://localhost:3000",
+    "http://localhost:3001",
+    "http://localhost:3002", // add more origins
   ],
   methods: "*",
 };
